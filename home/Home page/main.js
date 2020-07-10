cb2a8fead321c5a7af2e59f3582c38117aa8af87
Quazi-Reza <48997433+Quazi-Reza@users.noreply.github.com>
2020-07-10 17:04:00 +0600

    photo gallery

---
+++ home/Home page/main.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+
+        $("#owl-demo").owlCarousel({
+
+            autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+            items : 4,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3]
+
+    });
+
+  });
+ 
+
+$(document).ready(function() {
+     
+    var owl = $(".owl-carousel");
+   
+    owl.owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+            items : 4,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3]
+    });
+   
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+      })
+      $(".prev").click(function(){
+        owl.trigger('owl.prev');
+      })
+  });
+
+
